c42ea3fc16332033c15a64222cc595cb3674155d
naughtaught <naughtaught@protonmail.com>
2026-08-05 06:12:14 -0600

    changed ambiguous text

--- src/lib/components/modals/HomeMenu.svelte
+++ src/lib/components/modals/HomeMenu.svelte
@@ -12,6 +12,6 @@
     }}
     class="fixed top-12 right-10 z-30 flex h-10 w-48 -translate-y-1/2 rounded-md bg-backgroundColor px-2 py-2">
     <div class="mt-1 ml-5">
-        <Checkbox label="Include Hidden" bind:checked={$showHomeHidden} func={() => {}} />
+        <Checkbox label="Show All" bind:checked={$showHomeHidden} func={() => {}} />
     </div>
 </div>
